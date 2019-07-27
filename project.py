bb50ed5559763ad0a7897f26eeef75932247d34b
Bai Biram Njie Fog <baibiramnjie41@gmail.com>
2019-07-27 13:34:27 -0400

    Initial commit

---
+++ project.py
@@ -0,0 +1,36 @@
+import time
+print("you find yourself in  brikama")
+time.sleep(1)
+print("with a mad man following you with a stick")
+time.sleep(1)
+print("in front of you are two junctions")
+time.sleep(1)
+print("one is called jaliba junction")
+time.sleep(1)
+print("the other one is called box bar junction")
+time.sleep(1)
+print("choose a junction")
+time.sleep(1)
+while True:
+    response = input("enter 1 to enter jaliba\n""enter 2 to enter box bar\n").lower()
+    if '1'==res:
+        print("you entered but its still following you\n")
+        time.sleep(2)
+    x=input("choose (1)to run and (2) to enter the  door in front of you" )
+    if '1' == x:
+        print("you just ran but suddenly you saw a car\n")
+    elif '2' == x:
+        print("oops!!you knocked on the door but the mad man opened and ate you up\n")
+    else:
+        print("press (1) to fight it")
+    break
+
+
+
+                    
+    car=input("press (1) to enter the car\n")
+    if '1' == car:
+            print("wow you disapperard!!")
+    
+                 
+    
